9a1f3d5df260e310c66a2b29a966535d289e9675
Mark Sarpy <37461756+Sarpyjr@users.noreply.github.com>
2018-03-19 09:54:34 -0500

    Update timeFormatter.js

--- timeFormatter.js
+++ timeFormatter.js
@@ -1,112 +1,142 @@
-function timeFormatter(myTime) {
+    function timeFormatter(myTime) {
 
-	timeCheckerMessage.innerHTML = "";
+//If delete key or backspace key is press clear text box
+var key = event.keyCode || event.charCode;
+if( key == 8 || key == 46 ){
+    myTime.value = "";
+}
 
-	//If delete key or backspace key is press clear text box
-	var key = event.keyCode || event.charCode;
-    if( key == 8 || key == 46 ){
-		myTime.value = "";
-	}
-	
-	//Chars allowed
-	var ValidChars = /[^0-9^:^A^P^a^p^ ]/gi
-	if(ValidChars.test(myTime.value)) {
-		myTime.value = myTime.value.replace(ValidChars,"");
-	}
-		
-	//Pad 0 and add :
-	if(myTime.value.length == 1){
-		if(myTime.value.trim() == 2 || myTime.value.trim() == 3 || myTime.value.trim() == 4 || myTime.value.trim() == 5 || myTime.value.trim() == 6 || myTime.value.trim() == 7 || myTime.value.trim() == 8 || myTime.value.trim() == 9){
-			myTime.value = "0" + myTime.value + ":";
-			}
-	}
-	
-	//If 1st Char is not numeric
-	if(myTime.value.length == 1){
-		if(isNaN(parseInt(myTime.value))){
-			myTime.value = "";
-		}
-	}
-	
-	//If not numeric
-	if(myTime.value.length == 2){
-		if(isNaN(myTime.value)){
-			myTime.value = myTime.value;
-		}
-	}
+//Chars allowed
+var ValidChars = /[^0-9^:^A^P^a^p^ ]/gi
+if(ValidChars.test(myTime.value)) {
+    myTime.value = myTime.value.replace(ValidChars,"");
+    timeCheckerMessage.innerHTML = "Please enter valid time format, example 12:59 PM.";
+}
+    
+//Pad 0 and add :
+if(myTime.value.length == 1){
+    if(myTime.value.trim() == 2 || myTime.value.trim() == 3 || myTime.value.trim() == 4 || myTime.value.trim() == 5 || myTime.value.trim() == 6 || myTime.value.trim() == 7 || myTime.value.trim() == 8 || myTime.value.trim() == 9){
+        myTime.value = "0" + myTime.value + ":";
+        }
+}
 
-	//Checks if hour is numeric
-	if(myTime.value.length == 2){
-		if(!isNaN(myTime.value.substring(0, 2)) == false){
-			myTime.value = myTime.value.substring(0, myTime.value.length-1);
-		} else {
-			myTime.value = myTime.value + ":";
-		}
-	}
-	
-	//Checks if minutes is numeric
-	if(myTime.value.length == 5){
-		if(!isNaN(myTime.value.substring(4, 5)) == false){
-			myTime.value = myTime.value.substring(0, myTime.value.length-2);
-		}
-	}
+//If 1st Char is not numeric
+if(myTime.value.length == 1){
+    if(isNaN(parseInt(myTime.value))){
+        myTime.value = "";
+        timeCheckerMessage.innerHTML = "Please enter 12 hour format.";
+    }
+}
 
-	//Clear text box if hour is greater the 12
-	if(myTime.value.length == 5){
-		if(parseInt(myTime.value.substring(3).trim()) >= 60){
-			myTime.value = myTime.value.substring(0,3);
-			return;
-		} 
-	}	
-	
-	//After minutes add space
-	if(myTime.value.length == 5){
-		myTime.value = myTime.value + " ";
-    }	
-	
-	//Add PM when user press p or a and remove anything thats not p or a
-	if(myTime.value.length > 6){
-		if(myTime.value.substring(6, 8) == "p" || myTime.value.substring(6, 8) == "P"){	
-			if(myTime.value.substring(6, 8) == "p"){
-				myTime.value = myTime.value.replace("p","PM");
-				myTime.blur();	
-			}
-			if(myTime.value.substring(6, 8) == "P"){
-				myTime.value = myTime.value.replace("P","PM");
-				myTime.blur();	
-			}			
-		} else if(myTime.value.substring(6, 8) == "a" || myTime.value.substring(6, 8) == "A"){
-			if(myTime.value.substring(6, 8) == "a"){
-				myTime.value = myTime.value.replace("a","AM");
-				myTime.blur();	
-			}
-			if(myTime.value.substring(6, 8) == "A"){
-				myTime.value = myTime.value.replace("A","AM");
-				myTime.blur();	
-			}			
-		} else {
-		myTime.value = myTime.value.substring(0, myTime.value.length-1);
-		}		
-	}
-	
+//If not numeric
+if(myTime.value.length == 2){
+    if(isNaN(myTime.value)){        
+        myTime.value = myTime.value;
+        timeCheckerMessage.innerHTML = "Please enter 12 hour format.";       
+    }
+}
+
+//Checks if hour is numeric
+if(myTime.value.length == 2){
+    if(!isNaN(myTime.value.substring(0, 2)) == false){
+        myTime.value = myTime.value.substring(0, myTime.value.length-1);        
+    } else {
+        if(parseInt(myTime.value.substring(0, 2).trim()) >= 13){
+            myTime.value = myTime.value.substring(0, myTime.value.length-2);
+            timeCheckerMessage.innerHTML = "Please enter 12 hour format.";
+            return;
+        } else {
+            myTime.value = myTime.value + ":";
+        }			
+    }
+}
+
+//Check if hour and : is located
+if(myTime.value.length == 3){
+    if(myTime.value.substring(2, 3) != ":"){
+        myTime.value = "";
+        timeCheckerMessage.innerHTML = "Please enter 12 hour format.";
+        return;
+    }
+}
+
+//Checks if minutes is numeric
+if(myTime.value.length == 5){
+    if(!isNaN(myTime.value.substring(3, 5)) == false){
+        myTime.value = myTime.value.substring(0, myTime.value.length-2);
+        timeCheckerMessage.innerHTML = "Please enter valid minute format 00 - 59.";
+    } 
+}
+
+//Checks if minutes are higher then 59
+if(myTime.value.length == 5){
+    if(parseInt(myTime.value.substring(3, 5).trim()) >= 60){
+        myTime.value = myTime.value.substring(0, myTime.value.length-2);
+        timeCheckerMessage.innerHTML = "Please enter valid minute format 00 - 59.";
+    }
+}
+
+//After minutes add space
+if(myTime.value.length == 5){
+    myTime.value = myTime.value + " ";
+}
+
+//Check if hour and : is located
+if(myTime.value.length == 6){
+    if(myTime.value.substring(5, 6) != " "){
+        myTime.value = myTime.value.substring(0, myTime.value.length-3);
+        timeCheckerMessage.innerHTML = "Please enter valid minute format 00 - 59.";
+    }
+}
+
+//Add PM when user press p or a and remove anything thats not p or a
+if(myTime.value.length > 6){
+    if(myTime.value.substring(6, 8) == "p" || myTime.value.substring(6, 8) == "P"){	
+        if(myTime.value.substring(6, 8) == "p"){
+            myTime.value = myTime.value.replace("p","PM");
+            myTime.blur();
+            timeCheckerMessage.innerHTML = "";	
+        }
+        if(myTime.value.substring(6, 8) == "P"){
+            myTime.value = myTime.value.replace("P","PM");
+            myTime.blur();	
+            timeCheckerMessage.innerHTML = "";
+        }			
+    } else if(myTime.value.substring(6, 8) == "a" || myTime.value.substring(6, 8) == "A"){
+        if(myTime.value.substring(6, 8) == "a"){
+            myTime.value = myTime.value.replace("a","AM");
+            myTime.blur();
+            timeCheckerMessage.innerHTML = "";	
+        }
+        if(myTime.value.substring(6, 8) == "A"){
+            myTime.value = myTime.value.replace("A","AM");
+            myTime.blur();
+            timeCheckerMessage.innerHTML = "";	
+        }			
+    } else {
+    myTime.value = myTime.value.substring(0, myTime.value.length-1);
+    timeCheckerMessage.innerHTML = "Please type 'a' for AM or 'p' for PM.";
+    }		
+}
+
 
 }
 
 function timeChecker(myTime) {
-	if (!isNaN(myTime.value.substring(0, 2)) == false){
-		myTime.value = "";
-		timeCheckerMessage.innerHTML = "Please enter valid time format"
-	}
-	if (!isNaN(myTime.value.substring(4, 5)) == false){
-		myTime.value = "";
-		timeCheckerMessage.innerHTML = "Please enter valid time format"
-	}
-	if (myTime.value.substring(6, 8) == "PM"){
+if (!isNaN(myTime.value.substring(0, 2)) == false){
+    myTime.value = "";
+    timeCheckerMessage.innerHTML = "Please enter valid time format, example 12:59 PM.";
+}
+if (!isNaN(myTime.value.substring(4, 5)) == false){
+    myTime.value = "";
+    timeCheckerMessage.innerHTML = "Please enter valid time format, example 12:59 PM.";
+}
+if (myTime.value.substring(6, 8) == "PM"){
 
-	} else if (myTime.value.substring(6, 8) == "AM") {
-	} else {
-		myTime.value = "";
-		timeCheckerMessage.innerHTML = "Please enter valid time format"
-	}
-		
+} else if (myTime.value.substring(6, 8) == "AM") {
+} else {
+    myTime.value = "";
+    timeCheckerMessage.innerHTML = "Please enter valid time format, example 12:59 PM.";
+}
+    
 }
